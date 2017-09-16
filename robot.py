52b2ed06a08bc6c63b3699b31154aeaf52706df1
Michal Piotr Stankiewicz <stinger.dat@gmail.com>
2017-09-16 13:35:40 +0000

    add if __name...

--- robot.py
+++ robot.py
@@ -1,76 +1,82 @@
 import requests
 from requests.auth import HTTPDigestAuth
-import urllib
 import time
 from threading import Thread
 
-# Thanks to Phaiax for the code fixing  https://github.com/Phaiax/OhOnlyMusli/tree/master/pytest
 
-url = 'http://13.95.29.162/rw'
-auth = HTTPDigestAuth('Default User', 'robotics')
+class RobotMover(object):
+    def __init__(self, host='13.95.29.162', user='Default User', password='robotics'):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.url = 'http://{host}/rw'.format(host=host)
+        self.url = '{url}/rapid/symbol/data/RAPID'.format(url=self.url)
+        self.auth = HTTPDigestAuth(user, password)
 
-session = requests.session()
+        self.session = requests.session()
 
+        resp = self.session.get(
+            '{url}/T_ROB_R/Remote/bStart?json=1'.format(url=self.url),
+            auth=self.auth
+        )
 
-# Get authenticated
-r0 = session.get(url + '/rapid/symbol/data/RAPID/'+'T_ROB_R'+'/Remote/bStart?json=1',
-                   auth=auth)
-assert(r0.status_code == 200)
+        assert (resp.status_code == 200)
 
+    def check(self, arm, variable):
+        print(self.host)
+        r = self.session.get('{url}/{arm}/Remote/{variable}?json=1'
+                             .format(url=self.url, arm=arm, variable=variable))
+        assert (r.status_code == 200)
+        return r.json()['_embedded']['_state'][0]['value']
 
-def check(arm, variable):
-    r = session.get(url + '/rapid/symbol/data/RAPID/'+arm+'/Remote/'+variable+'?json=1')
-    assert(r.status_code == 200)
-    return r.json()['_embedded']['_state'][0]['value']
+    def check_bool(self, arm, variable):
+        return self.check(arm, variable) == "TRUE"
 
-def checkBool(arm, variable):
-    return True if check(arm, variable) == "TRUE" else False
+    def set_string(self, arm, variable, text):
+        payload = {'value': '"{text}"'.format(text=text)}
+        url = '{url}/{arm}/Remote/{variable}?action=set'.format(url=self.url, arm=arm, variable=variable)
+        r = self.session.post(url, data=payload)
+        print(url, r, r.text)
+        assert (r.status_code == 204)
+        return r
 
-def setString(arm, variable, text):
-    payload={'value':'"'+text+'"'}
-    r = session.post(url + '/rapid/symbol/data/RAPID/'+arm+'/Remote/'+variable+'?action=set',
-                     data=payload)
-    print(r)
-    print(r.text)
-    assert(r.status_code == 204)
-    return r
+    def set_bool(self, arm, variable, state):
+        payload = {'value': 'true' if state else 'false'}
+        url = '{url}/{arm}/Remote/{variable}?action=set'.format(url=self.url, arm=arm, variable=variable)
+        r = self.session.post(url, data=payload)
+        print(url, r, r.text)
+        assert (r.status_code == 204)
+        return r
 
-def setBool(arm, variable, state):
-    payload={'value': 'true' if state else 'false' }
-    r = session.post(url + '/rapid/symbol/data/RAPID/'+arm+'/Remote/'+variable+'?action=set',
-                     data=payload)
-    print(r)
-    print(r.text)
-    assert(r.status_code == 204)
-    return r
+    def move_robot(self, arm, action):
+        print("RUNNING:" + self.check(arm, 'bRunning'))
 
-def moveRobot(arm,action):
-    print("RUNNING:" + check(arm, 'bRunning'))
+        self.set_string(arm, 'stName', action)
 
-    setString(arm, 'stName', action)
+        print("bStart:" + self.check(arm, 'bStart'))
 
-    print("bStart:" + check(arm, 'bStart'))
+        self.set_bool(arm, 'bStart', True)
 
-    setBool(arm, 'bStart', True);
+        print("bStart:" + self.check(arm, 'bStart'))
+        time.sleep(0.5)
 
-    print("bStart:" + check(arm, 'bStart'))
-    time.sleep(0.5)
-
-    running = checkBool(arm, 'bRunning')
-    while running:
-        running = checkBool(arm, 'bRunning')
-        print("RUNNING:" + str(running))
-        time.sleep(0.4)
+        running = self.check_bool(arm, 'bRunning')
+        while running:
+            running = self.check_bool(arm, 'bRunning')
+            print("RUNNING:" + str(running))
+            time.sleep(0.4)
 
 
 # the trick seems to run both arms in parralel because of the
 # waitAsyncTask directive in the RAPID code
 
+
 class otherArm(Thread):
     def run(self):
-        moveRobot('T_ROB_L','NoClue')
+        RobotMover().move_robot('T_ROB_L', 'NoClue')
 
 
-other = otherArm()
-other.start()
-moveRobot('T_ROB_R','NoClue')
+if __name__ == '__main__':
+    other = otherArm()
+    other.start()
+    RobotMover().move_robot('T_ROB_R', 'NoClue')
