4ecf3accde93691872e1ff4d625d8ddf3125f302
Michal Stefanczyk <michal.stefanczyk@hotmail.com>
2017-09-16 23:51:13 +0200

    Added weather nowcast

--- app/app.js
+++ app/app.js
@@ -44,6 +44,7 @@ function getAccessToken(clientId, clientSecret, callback) {
 }
 
 function speechToText(filename, accessToken, callback) {
+   getWeatherForecast();
   //curl -v -X POST "https://speech.platform.bing.com/speech/recognition/interactive/cognitiveservices/v1?language=en-US&locale=en&format=txt&requestid=your_guid" -H "Transfer-Encoding: chunked" -H 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzY29wZSI6Imh0dHBzOi8vc3BlZWNoLnBsYXRmb3JtLmJpbmcuY29tIiwic3Vic2NyaXB0aW9uLWlkIjoiODYwNmNkMTAwZWExNGVhYzk2Njc0OTYyNTRhZDI1YmIiLCJwcm9kdWN0LWlkIjoiQmluZy5TcGVlY2guRjAiLCJjb2duaXRpdmUtc2VydmljZXMtZW5kcG9pbnQiOiJodHRwczovL2FwaS5jb2duaXRpdmUubWljcm9zb2Z0LmNvbS9pbnRlcm5hbC92MS4wLyIsImF6dXJlLXJlc291cmNlLWlkIjoiL3N1YnNjcmlwdGlvbnMvNzEzMDI3ZGUtYjYzZS00YjYxLWFjYjktZjM1N2VjY2M0MTU5L3Jlc291cmNlR3JvdXBzL0EvcHJvdmlkZXJzL01pY3Jvc29mdC5Db2duaXRpdmVTZXJ2aWNlcy9hY2NvdW50cy9TcGVhY2giLCJpc3MiOiJ1cm46bXMuY29nbml0aXZlc2VydmljZXMiLCJhdWQiOiJ1cm46bXMuc3BlZWNoIiwiZXhwIjoxNTA1NTY3NjY5fQ.be2Sy4JvzuR_LtAVJp5XyVOtTXm_L_YIRqrieD1UEnw' -H 'Content-type: audio/wav; codec="audio/pcm"; samplerate=16000' --data-binary @t.wav
   fs.readFile(filename, function(err, waveData) {
     if(err) return callback(err);
@@ -142,10 +143,32 @@ function initRobotSubprocess() {
 
 
 function getWeatherForecast() {
-    return "It looks like it will rain today...";
+    var lat = 37.56
+    var lon = 56.67
+
+    request.get({
+        url: 'https://api.us.apiconnect.ibmcloud.com' + '/infomichaelwellnerde-dev/hackzurich/v1/geocode/'+lat+'/'+lon+'/forecast/nowcast.json?units=e',
+        headers: {
+            'accept': 'application/json',
+            'X-IBM-Client-Id': '7216b8fc-4815-43e4-bc20-1614e35aec09',
+            'X-IBM-Client-Secret': 'iX0dP6tS0jA0tR6uR5yH6eO7pV6bS0fL4gW8mW6gI5kG8rU2nU',
+        },
+    }, function(err, resp, body) {
+       if (err) return callback(err);
+       var data = JSON.parse(body);
+       var res;
+       if(data && data.forecast && data.forecast.narrative_128char)
+         res = "The weather is " + data.forecast.narrative_128char;
+       else
+         res = "It looks like it will rain today...";
+        luisAction({
+            'response': res,
+            'movement': null,
+        });
+    });
 }
 
-function detectAction(luisRes) {
+function doAction(luisRes) {
     var intent = luisRes['topScoringIntent']['intent'];
     var intent_mappings = {
         'BadJob': {
@@ -177,16 +200,10 @@ function detectAction(luisRes) {
             'response': "Whatever you ask, sir."
         },
         'Weather.GetCondition': function() {
-            return {
-                'movement': null,
-                'response': getWeatherForecast()
-            };
+            getWeatherForecast(luisAction);
         },
         'Weather.GetForecast': function() {
-            return {
-                'movement': null,
-                'response': getWeatherForecast()
-            };
+            getWeatherForecast(luisAction);
         },
         'None': {
             'movement': 'NoClue',
@@ -196,9 +213,10 @@ function detectAction(luisRes) {
     var intent_func_or_dict = intent_mappings['None'];
     if (intent_mappings[intent]) intent_func_or_dict = intent_mappings[intent];
     if (typeof intent_func_or_dict === "function") {
-        return intent_func_or_dict();
+        intent_func_or_dict();
+    } else {
+        luisAction(intent_func_or_dict);
     }
-    return intent_func_or_dict
 }
 
 function sendActionToRobot(action) {
@@ -249,18 +267,19 @@ app.post('/recognize', function(req, res) {
   req.pipe(busboy);
 });
 
+function luisAction(action) {
+    console.log(action);
+    if (action.movement) {
+        sendActionToRobot(action.movement);
+    }
+    textToSpeech(action.response);
+}
+
 app.get('/luis', function(req, res) {
   LUIS(req.query.q, function(err, luisres) {
       if(err) return console.log(err);
-
-        console.log(luisres);
-        var action = detectAction(luisres);
-        if (action.movement) {
-            sendActionToRobot(action.movement);
-        }
-        textToSpeech(action.response);
-
-        res.status(200).send(luisres);
+      doAction(luisres);
+      res.status(200).send(luisres);
     });
 });
 
